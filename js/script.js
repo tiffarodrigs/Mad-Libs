ce2ae62c1041aad42b1f6bd0c3044805db8f44e2
Tiffany Rodrigo <tiffa.rodrigs@gmail.com>
2022-01-12 08:24:38 -0800

    Add files for mad lib project

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    const person1Input = $("input#person1").val();
+    const person2Input = $("input#person2").val();
+    const animalInput= $("input#animal").val();
+    const exclamationInput = $("input#exclamation").val();
+    const verbInput = $("input#verb").val();
+    const nounInput = $("input#noun").val();
+
+    $(".person1").text(person1Input);
+    $(".person2").text(person2Input);
+    $(".animal").append(animalInput);
+    $(".exclamation").append(exclamationInput);
+    $(".verb").append(verbInput);
+    $(".noun").append(nounInput);
+
+    $("#story").show();
+
+    event.preventDefault();
+  });
+});
+
